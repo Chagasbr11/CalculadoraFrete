3e098441645cbb25daa9e9a04b4a219e841939a0
Chagasbr11 <114256601+Chagasbr11@users.noreply.github.com>
2023-10-09 23:13:18 -0300

    Add files via upload

---
+++ Calculadorafrete/Calculadora/src/Univas/edu/br/CalculadorFrete.java
@@ -0,0 +1,25 @@
+package Univas.edu.br;
+
+public class CalculadorFrete {
+    double freteProduto;
+
+
+    public double calcularCustoEnvio(double peso, double altura, double largura, double preço, int tipoEnvio){
+        if( tipoEnvio == 1){
+            freteProduto = peso * 2;
+        }
+        else if(tipoEnvio == 2){
+            freteProduto = altura * largura;
+        }
+        else if (tipoEnvio == 3) {
+            if(preço > 500){
+                freteProduto = 0;
+            }
+            else{
+                freteProduto = 20;
+            }
+        }
+        return freteProduto;
+    }
+
+}
---
+++ Calculadorafrete/Calculadora/src/Univas/edu/br/Produto.java
@@ -0,0 +1,39 @@
+package Univas.edu.br;
+
+public class Produto {
+    private double peso;
+    private double altura;
+    private double largura;
+    private double preço;
+    private int tipoEnvio;
+
+    public Produto(double peso, double altura, double largura, double preço, int tipoEnvio) {
+        this.peso = peso;
+        this.altura = altura;
+        this.largura = largura;
+        this.preço = preço;
+        this.tipoEnvio = tipoEnvio;
+    }
+
+
+    public int getTipoEnvio() {
+        return tipoEnvio;
+    }
+
+    public double getPeso() {
+        return peso;
+    }
+
+    public double getAltura() {
+        return altura;
+    }
+
+    public double getLargura() {
+        return largura;
+    }
+
+    public double getPreço() {
+        return preço;
+    }
+
+}
